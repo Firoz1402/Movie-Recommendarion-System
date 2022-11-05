e820573827475f9967423c529748cb43b4039a6c
Firoz1402 <firozanjum537@gmail.com>
2022-11-06 02:08:53 +0530

    Model and Server added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Movie-Recommendarion-System
-A web app that suggests movies similar to a particular movie you select.
+A web app that suggests movies similar to a particular movie you select. This is a content based recommendor system which uses Cosine similarity Matrix using vectorisation of words using Bag of Words.
---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+def recommend(movie):
+    movie_index= movies[movies['title']==movie].index[0] # marking index of the movie 
+    distances = similarity[movie_index] 
+    movies_list= sorted(list(enumerate(distances)), reverse = True, key= lambda x:x[1])[1:6]
+    
+    recommended_movies =[]
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append((movies.iloc[i[0]].title))
+    
+    return recommended_movies
+    
+
+
+
+
+
+
+
+movies_dict= pickle.load(open('movie_dict.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+
+similarity = pickle.load(open('similarity_matrix.pkl','rb'))
+st.title('Movie Recommendations')
+
+selected_movie_name = st.selectbox(
+
+    'What Kind of Movies Do You Watch?',
+    movies['title'].values
+    )
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+
+    for i in recommendations:
+        st.write(i)
